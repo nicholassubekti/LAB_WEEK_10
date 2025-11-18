d811542a252b172845c5e3c20202d9ac2ce300d3
Samuel Nicholas <nicholassubekti09@gmail.com>
2025-11-18 14:52:26 +0700

    Commit No. 2: Implement LiveData to Sync Activity and Fragment UI

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -4,27 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
-    private var total: Int = 0
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        total = 0
-        updateText(total)
-        findViewById<Button>(R.id.button_increment).setOnClickListener {
-            incrementTotal()
-        }
-    }
 
-    private fun incrementTotal() {
-        total++
-        updateText(total)
+        prepareViewModel()
     }
 
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
             getString(R.string.text_total, total)
+    }
+
+    private fun prepareViewModel(){
+        // Observe the LiveData object
+        viewModel.total.observe(this, {
+            // Whenever the value of the LiveData object changes
+            // the updateText() is called, with the new value as the parameter
+            updateText(it)
+        })
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
+        }
     }
 }
